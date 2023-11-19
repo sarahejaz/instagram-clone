bbcc99149f1b45a3ccc80b17e43433b53c7e7408
Sarah <58132791+sarahejaz@users.noreply.github.com>
2023-11-18 20:04:01 -0500

    Update db.js
    Used secret for MongoDB URI

--- instagram-mern-backend/config/db.js
+++ instagram-mern-backend/config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
   connection_string:
-    'mongodb+srv://admin:instagrammern@cluster0.ujpbu.mongodb.net/instagramdb?retryWrites=true&w=majority',
+    ${{ secrets.MONGODB_URI }},
   database: 'instagramdb',
   imgBucket: 'photos',
 };
